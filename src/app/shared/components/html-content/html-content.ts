fdab15f0a321fba2f8b126ee37490354f22d11d1
Your Name <pantazisvastardis@yahoo.com>
2026-07-22 13:25:36 +0300

    aaa

--- src/app/shared/components/html-content/html-content.ts
+++ src/app/shared/components/html-content/html-content.ts
@@ -3,7 +3,6 @@ import { Component, ViewEncapsulation, input } from '@angular/core';
 @Component({
   selector: 'app-html-content',
   template: '<section class="html-content" [innerHTML]="html()"></section>',
-  styleUrl: './html-content.scss',
   encapsulation: ViewEncapsulation.None,
 })
 export class HtmlContent {
